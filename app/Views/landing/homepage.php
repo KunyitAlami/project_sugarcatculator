2d300acfbb18f1e4d2a07d5a69ea0f4dccfa96dc
Ghani Mudzakir <ghanimdzkr3@gmail.com>
2025-07-06 18:52:46 +0800

    first commit

---
+++ app/Views/landing/homepage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Sugar Catculator</title>
+</head>
+<body>
+    <?= view('navbar/navbar') ?>
+
+    <h1>Ini adalah halaman landing dari Website Sugar CatCulator</h1>
+</body>
+</html>
